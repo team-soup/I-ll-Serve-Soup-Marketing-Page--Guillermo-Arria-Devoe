7c710d11acdc2c6e51af87bdd22d5cacf8b526e0
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-06 17:00:41 -0500

    Building features component, also implemented .map array method

--- components/features/features.js
+++ components/features/features.js
@@ -1,8 +1,12 @@
 class Feature {
     constructor(domElement) {
-        this.domElement = domElement; 
+        this.domElement = domElement;
+
     }
 }
 
 let features = document.querySelectorAll('.feature');
 features.forEach(feature => new Feature(feature));
+
+// Implementing an array method
+Array.from(features).map(feature => feature.style.display = 'none')
